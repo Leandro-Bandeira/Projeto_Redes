3d05d014c01fcec0b9cb701da8ccc48e231dec0c
leandrobandeira <leoviolinist22@gmail.com>
2024-05-03 12:00:34 -0300

    ajeitando codigo do udp

--- Client_udp.py
+++ Client_udp.py
@@ -9,9 +9,8 @@ UDP_PORT = 50000
 class Client_udp:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Criação do socket UDP
-        #self.type_msg = 0b0000 # Valor do tipo da mensagem (requisição)
         self.type_msg = format(0, '04b')
-        print(self.type_msg)
+    
     # Função responsável por enviar a requisição para o servidor
     def send_request(self, type):
         
@@ -21,24 +20,29 @@ class Client_udp:
         # 3 A quantidade de respostas emitidas pelo servidor até o momento. 
         type_request = None 
         if type == 1:
-            #type_request = 0b0000
             type_request = format(0, '04b')
         elif type == 2:
-            #type_request = 0b0001
             type_request = format(1, '04b')
         else:
-            #type_request = 0b0010
             type_request = format(2, '04b')
 
         id = format(random.randint(1, 65535), '016b') # Gera valores aleatores entre os intervalos dados inclusivos
         msg_string = self.type_msg + type_request + id
         msg = int(msg_string, 2).to_bytes(-(-len(msg_string) // 8), byteorder='big') # Cria um byte-object para enviar como mensagem
-        print(msg)
-        #print(msg.decode('ascii'))
-         
-        #self.sock.sendto(b'\x00\x37\x51', (UDP_IP, UDP_PORT))
+        
+        #print(msg)
         self.sock.sendto(msg, (UDP_IP, UDP_PORT))
         data, addr = self.sock.recvfrom(1024)
+        #print(data)        
+        # format(byte, '08b') transforma um valor (byte) em uma string de 8 bits
+        # logo int(format(byte, '08b')) transforma a string de 8 bits em um inteiro
+        bytes_list_rcv = [int(format(byte, '08b'), 2) for byte in data]
+        # Para cada valor inteiro, transforma-o em ascii a partir do quarto byte que começa a mensagem
+        # até o final
+        if type != 3:
+            msg_rcv = ''.join([chr(values) for values in bytes_list_rcv[4:-1]])
+        else: 
+            msg_rcv = sum([values for values in bytes_list_rcv[4:-1]])
+            print(bytes_list_rcv)
     
-        print(f'Received message: {data}')
-
+        print(msg_rcv)
--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from Client_udp import Client_udp
 
 # Função responsável pela interação com o usuário
 def main():
-    
-    choice = int(input("""Escolha um dos tipos de requisição:
+    while(1):    
+        choice = int(input("""Escolha um dos tipos de requisição:
     1. Data e hora atual.
     2. Uma mensagem motivacional para o fim do semestre.
     3. A quantidade de respostas emitidas pelo servidor até o momento.
     4. Sair
     """))
 
-    if choice == 4:
-        exit(1)
+        if choice == 4:
+            exit(1)
 
-    client = Client_udp()
-    client.send_request(choice)
+        client = Client_udp()
+        client.send_request(choice)
     
 
 
