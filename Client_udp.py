1d006bc45e3beba5730ab7952cabe0e55ac9f712
Leandro Lucas <leoviolinist22@gmail.com>
2024-05-04 09:55:41 -0300

    terminando udp

--- Client_udp.py
+++ Client_udp.py
@@ -9,40 +9,35 @@ UDP_PORT = 50000
 class Client_udp:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Criação do socket UDP
-        self.type_msg = format(0, '04b')
     
     # Função responsável por enviar a requisição para o servidor
     def send_request(self, type):
-        
-        #Análisa o que o usuário deseja
-        # 1 Data e hora
-        # 2 Uma mensagem motivacional para o fim do semestre
-        # 3 A quantidade de respostas emitidas pelo servidor até o momento. 
+        # Analisa o que o usuário deseja
+        # Perceba que a notação utilizada é big endian, o mais significativo está a esquerda
+        # 00000000 (0x00)- Data e hora
+        # 00000001 (0x01)- Uma mensagem motivacional para o fim do semestre
+        # 00000010 (0x02)- A quantidade de respostas emitidas pelo servidor até o momento. 
         type_request = None 
         if type == 1:
-            type_request = format(0, '04b')
+            type_request = b'\x00' 
         elif type == 2:
-            type_request = format(1, '04b')
+            type_request = b'\x01' 
         else:
-            type_request = format(2, '04b')
+            type_request = b'\x02'
 
-        id = format(random.randint(1, 65535), '016b') # Gera valores aleatores entre os intervalos dados inclusivos
-        msg_string = self.type_msg + type_request + id
-        msg = int(msg_string, 2).to_bytes(-(-len(msg_string) // 8), byteorder='big') # Cria um byte-object para enviar como mensagem
-        
-        #print(msg)
-        self.sock.sendto(msg, (UDP_IP, UDP_PORT))
+        id = random.randint(1, 65535).to_bytes(2, "big") #Gera o numero aleatorio e o converte para bytes no padrão big endian
+        msg = type_request + id # Concatena as informações 
+        self.sock.sendto(msg, (UDP_IP, UDP_PORT)) #Envia as mensagens UDP para o servidor
         data, addr = self.sock.recvfrom(1024)
-        #print(data)        
-        # format(byte, '08b') transforma um valor (byte) em uma string de 8 bits
-        # logo int(format(byte, '08b')) transforma a string de 8 bits em um inteiro
-        bytes_list_rcv = [int(format(byte, '08b'), 2) for byte in data]
+        
         # Para cada valor inteiro, transforma-o em ascii a partir do quarto byte que começa a mensagem
         # até o final
         if type != 3:
-            msg_rcv = ''.join([chr(values) for values in bytes_list_rcv[4:-1]])
-        else: 
-            msg_rcv = sum([values for values in bytes_list_rcv[4:-1]])
-            print(bytes_list_rcv)
-    
+            msg_rcv = str(data[4:-1].decode("ascii")).rstrip()
+        else:
+            msg_rcv = 0
+            
+            data_value = data[4:] # Pega todos os valores da resposta 
+            for i in range(len(data_value)):
+                msg_rcv += int.from_bytes(data_value[i:i+1], byteorder='big') #Converte cada byte em inteiro de acordo com o big endian
         print(msg_rcv)
