bca57190f75659a0f59ce003de55903f279ae08f
JulianaDantass <localadmin@q6.int.logq.dev>
2024-05-06 16:33:45 -0300

    read me atualizado

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Projeto_Redes
+
+## O programa deve ser rodado com o comando: 
+
+sudo python3 main.py
+
+## Nesse programa, o usuário irá escolher se o cliente é UDP ou RAW e, após isso, o tipo de requisição.
--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 
 from Client_udp import Client_udp
+from client_raw import Client_raw
 
 # Função responsável pela interação com o usuário
 def main():
 
-    while(1):    
-        choice = int(input("""Escolha um dos tipos de requisição:
-        1. Data e hora atual.
-        2. Uma mensagem motivacional para o fim do semestre.
-        3. A quantidade de respostas emitidas pelo servidor até o momento.
-        4. Sair
+    choice_client = int(input("""Escolha um dos tipos de cliente:
+        1. Cliente UDP.
+        2. Cliente RAW.
         """))
 
-        if choice == 4:
-            exit(1)
+    if choice_client == 1: 
 
-        client = Client_udp()
-        client.send_request(choice)
+        while(1):    
+            choice = int(input("""Escolha um dos tipos de requisição:
+            1. Data e hora atual.
+            2. Uma mensagem motivacional para o fim do semestre.
+            3. A quantidade de respostas emitidas pelo servidor até o momento.
+            4. Sair
+            """))
+
+            if choice == 4:
+                exit(1)
+
+            client = Client_udp()
+            client.send_request(choice)
+
+
+    else: 
+        while(1):    
+            choice = int(input("""Escolha um dos tipos de requisição:
+            1. Data e hora atual.
+            2. Uma mensagem motivacional para o fim do semestre.
+            3. A quantidade de respostas emitidas pelo servidor até o momento.
+            4. Sair
+            """))
+
+            if choice == 4:
+                exit(1)
+
+            client = Client_raw()
+            client.send_request()
+
+    
         
 
 if __name__ == "__main__":
