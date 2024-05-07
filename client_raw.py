2d750e2bcdc844f2648d27e7ee1284bf7ca8cbba
Juliana Dantas <juliana@q5.int.logq.dev>
2024-05-07 17:54:33 -0300

    tentando receber msg teste6

--- client_raw.py
+++ client_raw.py
@@ -61,7 +61,7 @@ class Client_raw:
         # para cada valor inteiro, transforma-o em ascii a partir do primeiro byte (onde a msg começa) depois de retirar os 32 bytes (cabeçalho fake, cabecalho real e os 4 bytes da outra parte) 
         # até o final
         if type != 3:   #se for uma mensagem (tipo string)
-            msg_rcv = str(data[32:-1].decode("ascii")).rstrip()
+            msg_rcv = str(data[28:-1].decode("ascii")).rstrip()
         
         else:           #se for a qtd de mensagens enviadas(tipo int)         
             msg_rcv = int.from_bytes((data[4:]), "big")
